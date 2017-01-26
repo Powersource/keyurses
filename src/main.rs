88bb54b0e49bd9c1c6213c4e33b7068ebad70586
Jacob Karlsson <jacob.karlsson95@gmail.com>
2017-01-26 22:43:54 +0100

    Add changing the wordfield

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,19 @@ extern crate cursive;
 
 use cursive::Cursive;
 use cursive::views::{Dialog, TextView, EditView, LinearLayout};
+use cursive::traits::Identifiable;
 
 fn main() {
     let mut siv = Cursive::new();
 
+    siv.add_global_callback('q', |s| s.quit());
+
     siv.add_layer(Dialog::around(LinearLayout::vertical()
-            .child(TextView::new("testtext"))
-            .child(EditView::new()))
-        .title("Cursive")
+            .child(TextView::new("testtext").with_id("word_field"))
+            .child(EditView::new().on_edit(|s, text, _| {
+                s.find_id::<TextView>("word_field").unwrap().set_content(text)
+            })))
+        .title("Keyurses")
         .button("Quit", |s| s.quit()));
 
     siv.run();
