eae49ccdb938b7a23c87a3b556aee910cdb21c7f
Jacob Karlsson <jacob.karlsson95@gmail.com>
2017-01-26 16:49:39 +0100

    Cursive Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate cursive;
+
+use cursive::Cursive;
+use cursive::views::{Dialog, TextView};
+
 fn main() {
-    println!("Hello, world!");
+    let mut siv = Cursive::new();
+
+    siv.add_layer(Dialog::around(TextView::new("Hello World"))
+    .title("Cursive")
+    .button("Quit", |s| s.quit()));
+
+    siv.run();
 }
