604cd6ebd247c6c3de8f7595ebf553ed3a322842
Jacob Karlsson <jacob.karlsson95@gmail.com>
2017-01-26 20:27:40 +0100

    Add text and editview

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 extern crate cursive;
 
 use cursive::Cursive;
-use cursive::views::{Dialog, TextView};
+use cursive::views::{Dialog, TextView, EditView, LinearLayout};
 
 fn main() {
     let mut siv = Cursive::new();
 
-    siv.add_layer(Dialog::around(TextView::new("Hello World"))
-    .title("Cursive")
-    .button("Quit", |s| s.quit()));
+    siv.add_layer(Dialog::around(LinearLayout::vertical()
+            .child(TextView::new("testtext"))
+            .child(EditView::new()))
+        .title("Cursive")
+        .button("Quit", |s| s.quit()));
 
     siv.run();
 }
