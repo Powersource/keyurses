0a14ed6b21b3b7c80393cd4ca82206dbf7832a14
Jacob Karlsson <jacob.karlsson95@gmail.com>
2017-01-27 02:22:45 +0100

    Add random word grabbing from the most used 10k
    The code is a bit messy right now but it works. rng.choose() was
    so nice that I think I can remove the struct now.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,43 @@
 extern crate cursive;
+extern crate rand;
 
 use cursive::Cursive;
 use cursive::views::{Dialog, TextView, EditView, LinearLayout};
 use cursive::traits::Identifiable;
+use rand::Rng;
 
 fn main() {
+    // This really messes with stdout. Seems to disable it by default but when
+    // siv is running println prints in random places on the screen.
     let mut siv = Cursive::new();
-
     siv.add_global_callback('q', |s| s.quit());
 
+    let words = Words::new();
+    
+
     siv.add_layer(Dialog::around(LinearLayout::vertical()
-            .child(TextView::new("testtext").with_id("word_field"))
-            .child(EditView::new().on_edit(|s, text, _| {
-                s.find_id::<TextView>("word_field").unwrap().set_content(text)
-            })))
-        .title("Keyurses")
-        .button("Quit", |s| s.quit()));
+                .child(TextView::new("testtext").with_id("word_field"))
+                .child(EditView::new().on_edit(move |s, _, _| {
+                    s.find_id::<TextView>("word_field").unwrap().set_content(words.rand_word())
+                })))
+            .title("Keyurses")
+            .button("Quit", |s| s.quit()));
 
     siv.run();
+}
+
+#[derive(Debug)]
+struct Words {
+    list: Vec<&'static str>,
+}
+
+impl Words {
+    fn new() -> Self {
+        Words { list: include_str!("google-10000-english-usa.txt").lines().collect() }
+    }
+
+    fn rand_word(&self) -> &str {
+        let mut rng = rand::thread_rng();
+        rng.choose(&self.list).unwrap()
+    }
 }
---
+++ word-credits.md
@@ -0,0 +1,4 @@
+google-10000-english-usa.txt is from https://github.com/first20hours/google-10000-english on 2017-01-27  
+This is what they call their license file:
+
+Data files are derived from the *Google Web Trillion Word Corpus*, as described by [Thorsten Brants and Alex Franz](http://googleresearch.blogspot.com/2006/08/all-our-n-gram-are-belong-to-you.html), and distributed by the [Linguistic Data Consortium](http://www.ldc.upenn.edu/Catalog/CatalogEntry.jsp?catalogId=LDC2006T13). Subsets of this corpus distributed by [Peter Novig](http://norvig.com/ngrams/). Corpus editing and cleanup by Josh Kaufman.
