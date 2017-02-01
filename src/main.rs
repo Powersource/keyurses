82dd0c2eb4c3e24d867febf8b199df5e24aecc34
Jacob Karlsson <jacob.karlsson95@gmail.com>
2017-02-01 17:07:10 +0100

    Borrow checker blues

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ fn main() {
     let mut siv = Cursive::new();
     siv.add_global_callback('q', |s| s.quit());
 
-    let words = WordBar::new();
+    let mut words = WordBar::new();
 
     siv.add_layer(Dialog::around(LinearLayout::vertical()
-            .child(TextView::new(words.rand_word()).with_id("target_field"))
+            .child(TextView::new(words.update_and_get_bar()).with_id("target_field"))
             .child(EditView::new()
-                .on_edit(move |s, input, _| typed_some(s, input, &words))
+                .on_edit(move |s, input, _| typed_some(s, input, words))
                 .with_id("input_field")))
         .title("Keyurses")
         .button("Quit", |s| s.quit()));
@@ -25,7 +25,7 @@ fn main() {
     siv.run();
 }
 
-fn typed_some(siv: &mut Cursive, input: &str, words: &WordBar) {
+fn typed_some(siv: &mut Cursive, input: &str, mut words: WordBar) {
     // See https://github.com/gyscos/Cursive/issues/102
     // for discussion on this mess
 
@@ -33,7 +33,7 @@ fn typed_some(siv: &mut Cursive, input: &str, words: &WordBar) {
     {
        let target_word = siv.find_id::<TextView>("target_field").unwrap();
        if target_word.get_content() == input {
-           target_word.set_content(words.rand_word());
+           target_word.set_content(words.update_and_get_bar());
            reset_input = true;
        }
     }
@@ -54,11 +54,31 @@ impl WordBar {
     fn new() -> Self {
         WordBar { 
             words: include_str!("google-10000-english-usa.txt").lines().collect(),
-            target_list: vec!("foo") }
+            target_list: vec!["foo"] }
     }
 
-    fn rand_word(&self) -> &str {
+    fn rand_word(&self) -> &'static str {
         let mut rng = rand::thread_rng();
         rng.choose(&self.words).unwrap()
+    }
+
+    fn update_and_get_bar(&mut self) -> String {
+        if self.target_list.len() > 0 {
+            self.target_list.pop();
+        }
+        while self.target_list.len() < 5 {
+            let new_word = self.rand_word();
+            self.target_list.push(new_word);
+        }
+        let mut bar_text: String = "".to_string();
+        for word in &self.target_list {
+            if bar_text == "" {
+                bar_text = word.to_string();
+            } else {
+                bar_text.push_str(" ");
+                bar_text.push_str(word);
+            }
+        }
+        bar_text
     }
 }
