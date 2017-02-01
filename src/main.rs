633ed60546a5141396389f53ef5304651bf2aefa
Jacob Karlsson <jacob.karlsson95@gmail.com>
2017-02-01 22:13:23 +0100

    Move things, not that it helps

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn main() {
     siv.add_layer(Dialog::around(LinearLayout::vertical()
             .child(TextView::new(words.update_and_get_bar()).with_id("target_field"))
             .child(EditView::new()
-                .on_edit(move |s, input, _| typed_some(s, input, words))
+                .on_edit(move |s, input, _| words.typed_some(s, input))
                 .with_id("input_field")))
         .title("Keyurses")
         .button("Quit", |s| s.quit()));
@@ -25,22 +25,6 @@ fn main() {
     siv.run();
 }
 
-fn typed_some(siv: &mut Cursive, input: &str, mut words: WordBar) {
-    // See https://github.com/gyscos/Cursive/issues/102
-    // for discussion on this mess
-
-    let mut reset_input = false;
-    {
-        let target_word = siv.find_id::<TextView>("target_field").unwrap();
-        if target_word.get_content() == input {
-            target_word.set_content(words.update_and_get_bar());
-            reset_input = true;
-        }
-    }
-    if reset_input {
-        siv.find_id::<EditView>("input_field").unwrap().set_content("");
-    }
-}
 
 type WordList = Vec<&'static str>;
 
@@ -55,6 +39,23 @@ impl WordBar {
         WordBar {
             words: include_str!("google-10000-english-usa.txt").lines().collect(),
             target_list: vec!["foo"],
+        }
+    }
+
+    fn typed_some(&mut self, siv: &mut Cursive, input: &str) {
+        // See https://github.com/gyscos/Cursive/issues/102
+        // for discussion on this mess
+
+        let mut reset_input = false;
+        {
+            let target_word = siv.find_id::<TextView>("target_field").unwrap();
+            if target_word.get_content() == input {
+                target_word.set_content(self.update_and_get_bar());
+                reset_input = true;
+            }
+        }
+        if reset_input {
+            siv.find_id::<EditView>("input_field").unwrap().set_content("");
         }
     }
 
